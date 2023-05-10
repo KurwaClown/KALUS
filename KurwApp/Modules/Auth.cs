9312fc065e7cd966c7e874f7301de4c3cdc4d685
KurwaClown <CobraClown09300@outlook.fr>
2023-05-11 01:07:39 +0200

    Refractored Authentication

--- KurwApp/Modules/Auth.cs
+++ KurwApp/Modules/Auth.cs
@@ -1,22 +1,33 @@
 ﻿using System.IO;
 using System.Text;
 
-namespace League
+namespace KurwApp
 {
     internal class Auth
     {
         private static int port = 0;
         private static string basic = "";
 
+        #region Authentication Set and Reset
+        //Set the basic authentication and the port for the requests
         internal static void SetBasicAuth(string filename)
-		{
-			string[] lockfile_content = GetLockfileContent(filename);
+        {
+            string[] lockfile_content = GetLockfileContent(filename);
 
-			port = int.Parse(lockfile_content[2]);
-			basic = Base64Encode($"riot:{lockfile_content[3]}");
-		}
+            port = int.Parse(lockfile_content[2]);
+            basic = Base64Encode($"riot:{lockfile_content[3]}");
+        }
 
-		private static string[] GetLockfileContent(string filename)
+        //Reseting the authentication values
+        internal static void ResetAuth()
+        {
+            port = 0;
+            basic = "";
+        } 
+        #endregion
+
+        //Reading the lockfile and returning each value contained as a string array
+        private static string[] GetLockfileContent(string filename)
 		{
 			string[] lockfile_content;
 			int x = filename.IndexOf("LeagueClientUx.exe");
@@ -33,12 +44,8 @@ namespace League
 			return lockfile_content;
 		}
 
-		internal static void ResetAuth()
-        {
-            port = 0;
-            basic = "";
-        }
 
+        #region Authentication Getter
         internal static string GetBasicAuth()
         {
             return basic;
@@ -47,14 +54,16 @@ namespace League
         internal static int GetPort()
         {
             return port;
-        }
+        } 
+        #endregion
 
+        //Returns a bool value defining if the authentication is set
         internal static bool IsAuthSet()
         {
             return (port != 0 && basic != "");
         }
 
-
+        //Encode a string to base64 (used for basic authentication)
         private static string Base64Encode(string plainText)
         {
             var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
