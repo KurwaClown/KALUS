a03557dd887fb8099dbf978a57198801f370349f
KurwaClown <CobraClown09300@outlook.fr>
2023-05-11 02:58:23 +0200

    Refractored Http Requests

--- KurwApp/Modules/Http_Request.cs
+++ KurwApp/Modules/Http_Request.cs
@@ -1,78 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace League
+namespace KurwApp
 {
 	internal static class Http_Request
 	{
-		internal static HttpClient _httpClient;
-
-		internal static void SetClient()
+		//Create a new HTTP Client
+		internal static HttpClient CreateClient()
 		{
-			HttpClientHandler handler = new HttpClientHandler();
-			handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
-			_httpClient = new HttpClient(handler);
+			HttpClientHandler handler = new HttpClientHandler
+			{
+				ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+			};
 
-			_httpClient.BaseAddress = new Uri($"https://127.0.0.1:{Auth.GetPort()}");
+			var httpClient = new HttpClient(handler)
+			{
+				BaseAddress = new Uri($"https://127.0.0.1:{Auth.GetPort()}")
+			};
 
-			_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Auth.GetBasicAuth());
+			httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Auth.GetBasicAuth());
+
+			return httpClient;
 		}
 
+		//Default HTTP GET Request
 		internal static async Task<HttpResponseMessage> GetRequest(string endpoint)
 		{
-			SetClient();
-			HttpResponseMessage result = await _httpClient.GetAsync(endpoint);
+			HttpClient client = CreateClient();
+			HttpResponseMessage result = await client.GetAsync(endpoint);
 			return result;
 		}
 
+		//Http Get Request for images
 		internal static async Task<byte[]> GetRequestImage(string endpoint)
 		{
-			SetClient();
+			HttpClient client = CreateClient();
 
-			HttpResponseMessage result = await _httpClient.GetAsync(endpoint);
+			HttpResponseMessage result = await client.GetAsync(endpoint);
 			return await result.Content.ReadAsByteArrayAsync();
 		}
 
+		//Default HTTP POST Request
 		internal static async Task<string> PostRequest(string endpoint, string body = "")
 		{
-			SetClient();
+			HttpClient client = CreateClient();
 
-			_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
 			var content = new StringContent(body, Encoding.UTF8, "application/json");
 
-			HttpResponseMessage result = await _httpClient.PostAsync(endpoint, content);
+			HttpResponseMessage result = await client.PostAsync(endpoint, content);
 
 			return await result.Content.ReadAsStringAsync();
 		}
 
-		
+		//Default HTTP PUT Request
 		internal static async Task<string> PutRequest(string endpoint, string body)
 		{
-			SetClient();
+			HttpClient client = CreateClient();
 
-			_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
 			var content = new StringContent(body, Encoding.UTF8, "application/json");
 
-			HttpResponseMessage result = await _httpClient.PutAsync(endpoint, content);
+			HttpResponseMessage result = await client.PutAsync(endpoint, content);
 
 			return await result.Content.ReadAsStringAsync();
 		}
 
+		//Default HTTP PATCH Request
 		internal static async Task<string> PatchRequest(string endpoint, string body) {
-			SetClient();
+			HttpClient client = CreateClient();
 
-			_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
 			var content = new StringContent(body, Encoding.UTF8, "application/json");
 
-			HttpResponseMessage result = await _httpClient.PatchAsync(endpoint, content);
+			HttpResponseMessage result = await client.PatchAsync(endpoint, content);
 
 			return await result.Content.ReadAsStringAsync();
 		}
