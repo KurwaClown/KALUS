f7a4eb5373336ab387b39f3d8ea3cba6f825b97c
KurwaClown <CobraClown09300@outlook.fr>
2023-05-20 02:57:28 +0200

    Adding ClientDataCache to store data changing on league update

---
+++ KurwApp/Modules/ClientDataCache.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace KurwApp.Modules
+{
+	internal static class ClientDataCache
+	{
+		private static JArray? championsInformation;
+		private static JArray? championsRunesRecommendation;
+
+		private static string? summonerId;
+
+		internal static async Task<JArray> GetChampionsInformations()
+		{
+			championsInformation ??= await Client_Request.GetChampionsInfo();
+
+			return championsInformation;
+		}
+
+		internal static async Task<JArray> GetChampionsRunesRecommendation()
+		{
+			championsRunesRecommendation ??= await Client_Request.GetRecommendedRunes();
+
+			return championsRunesRecommendation;
+		}
+
+		internal static async Task<string> GetSummonerId()
+		{
+			if (summonerId == null)
+			{
+				JObject summonerAndAccountId = await Client_Request.GetSummonerAndAccountId();
+				summonerId = summonerAndAccountId["summonerId"].ToString();
+			}
+
+			return summonerId;
+		}
+	}
+}
