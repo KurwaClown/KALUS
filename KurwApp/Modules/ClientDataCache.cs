5bc95c1bda78b0625de692aac03048bd2421119f
KurwaClown <CobraClown09300@outlook.fr>
2023-05-20 03:10:45 +0200

    Adding a variable reset for cached data when the client gets closed

--- KurwApp/Modules/ClientDataCache.cs
+++ KurwApp/Modules/ClientDataCache.cs
@@ -34,5 +34,13 @@ namespace KurwApp.Modules
 
 			return summonerId;
 		}
+
+		internal static void ResetCachedData()
+		{
+			championsInformation = null;
+			championsRunesRecommendation = null;
+
+			summonerId = null;
+		}
 	}
 }
