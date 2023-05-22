22f9390675adb66c6c3ac3951601e8465400ab12
KurwaClown <CobraClown09300@outlook.fr>
2023-05-22 18:12:48 +0200

    Adding inherited class from Game class

---
+++ KurwApp/Modules/Aram.cs
@@ -0,0 +1,122 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KurwApp.Modules
+{
+	internal class Aram : Game
+	{
+		internal Aram(MainWindow mainWindow)
+		{
+			this.mainWindow = mainWindow;
+		}
+
+		//Handler of the champion selections
+		internal override async Task ChampSelectControl()
+		{
+			sessionInfo = await Client_Request.GetSessionInfo();
+			if (sessionInfo is null) return;
+
+			mainWindow.SetGamemodeName("ARAM");
+			mainWindow.SetGameModeIcon("ARAM");
+
+			while (Auth.IsAuthSet())
+			{
+				sessionInfo = await Client_Request.GetSessionInfo();
+
+				if (sessionInfo is null) return;
+				switch (sessionInfo.Value<string>("timer.phase"))
+				{
+					default:
+						break;
+
+					case "FINALIZATION":
+						await Finalization();
+						break;
+
+					case "GAME_STARTING":
+					case "":
+						mainWindow.EnableRandomSkinButton(false);
+						return;
+				}
+				Thread.Sleep(1000);
+			}
+		}
+
+		protected override async Task Finalization()
+		{
+			if (Client_Control.GetSettingState("aramChampionSwap"))
+			{
+				int aramPick = GetBenchChampionPick();
+
+				if (aramPick != 0)
+				{
+					await Client_Request.AramBenchSwap(aramPick);
+					await PostPickAction();
+					championId = await Client_Request.GetCurrentChampionId();
+
+					if (Client_Control.GetSettingState("runesSwap") && !isRunePageChanged) await ChangeRunes();
+
+					if (Client_Control.GetSettingState("autoSummoner")) await ChangeSpells();
+				}
+			}
+			var currentChampionId = await Client_Request.GetCurrentChampionId();
+
+			if (currentChampionId != championId)
+			{
+				championId = currentChampionId;
+				await PostPickAction();
+				if (Client_Control.GetSettingState("runesSwap")) await ChangeRunes();
+
+				if (Client_Control.GetSettingState("autoSummoner")) await ChangeSpells();
+			}
+		}
+
+		//Get the pick the aram champion to pick if any
+		protected int GetBenchChampionPick()
+		{
+			var aramPicks = JArray.Parse(File.ReadAllText($"Picks/ARAM.json"));
+
+			if (!aramPicks.Any()) return 0;
+
+			List<int> aramBenchIds = GetAramBenchIds();
+			if (!aramBenchIds.Any()) return 0;
+
+			foreach (var pick in aramPicks)
+			{
+				if (aramBenchIds.Contains(pick.Value<int>())) return pick.Value<int>();
+			}
+			return 0;
+		}
+
+		//Get the champion benched (their id) in aram
+		protected List<int> GetAramBenchIds()
+		{
+			return sessionInfo["benchChampions"].Select(x => int.Parse(x["championId"].ToString())).ToList();
+		}
+
+		protected override async Task ChangeSpells()
+		{
+			var runesRecommendation = await Client_Control.GetSpellsRecommendationByPosition(championId, "NONE");
+
+			var spellsId = runesRecommendation.ToObject<int[]>();
+
+			if (!spellsId.Contains(32)) spellsId[1] = 32;
+
+			Client_Control.SetSummonerSpells(spellsId);
+		}
+
+		protected override async Task ChangeRunes()
+		{
+			bool isSetActive = (bool)Client_Control.GetPreference("runes.notSetActive");
+			string activeRunesPage = isSetActive ? (await Client_Request.GetActiveRunePage())["id"].ToString() : "0";
+			await Client_Control.SetRunesPage(championId, "NONE");
+			isRunePageChanged = true;
+
+			if (isSetActive) await Client_Request.SetActiveRunePage(activeRunesPage);
+		}
+	}
+}
