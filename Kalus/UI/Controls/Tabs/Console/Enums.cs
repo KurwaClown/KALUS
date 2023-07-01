e94bbfed7ccff1d6ba52db4599736c33735507c7
KurwaClown <CobraClown09300@outlook.fr>
2023-07-01 15:45:18 +0200

    Moving enums to a common file of the namespace

---
+++ Kalus/UI/Controls/Tabs/Console/Enums.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalus.UI.Controls.Tabs.Console
+{
+
+		public enum LogLevel
+		{
+			INFO,
+			WARN,
+			ERROR
+		}
+
+		public enum Utility
+		{
+			KALUS,
+			CLIENT,
+			RUNES,
+			SPELL,
+			SWAPPING,
+			READY,
+			SKIN,
+			GAME,
+			REROLL,
+			TRADE
+		}
+
+		public enum ClientState
+		{
+			NOCLIENT,
+			NONE,
+			LOBBY,
+			MATCHMAKING,
+			READYCHECK,
+			CHAMPSELECT,
+			GAMESTART
+		}
+}
