50db9b74c261e02ddfce16397e5217b50d07d306
KurwaClown <CobraClown09300@outlook.fr>
2023-07-14 02:37:29 +0200

    Creating a saveable checkbox component for application settings usage

---
+++ Kalus/UI/Controls/Components/SaveableCheckbox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Kalus.UI.Controls.Components
+{
+	internal class SaveableCheckbox : CheckBox
+	{
+		public SaveableCheckbox() {
+			this.Checked += SaveOnCheckedStateChange;
+			this.Unchecked += SaveOnCheckedStateChange;
+		}
+
+		private void SaveOnCheckedStateChange(object sender, System.Windows.RoutedEventArgs e)
+		{
+			Properties.Settings.Default.Save();
+		}
+	}
+}
